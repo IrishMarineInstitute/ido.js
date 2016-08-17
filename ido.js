db8d6e9d58fc910c76f7e53e82a55952bf6a8a54
Rob Fuller <fullergalway@gmail.com>
2016-08-17 17:09:07 +0100

    added more tide forecast widgets

--- ido.js
+++ ido.js
@@ -7206,7 +7206,25 @@ window.ido.mi.tides = window.ido.mi.tides || {};
 window.ido.mi.tides.galway = window.ido.mi.tides.galway || {};
 window.ido.mi.tides.galway.gauge = window.ido.mi.tides.galway.gauge || tides.gauge.bind(this,"Galway Port");
 const tidecast = require('./mi-tides-forecast-widget');
-window.ido.mi.tides.galway.forecast = window.ido.mi.tides.galway.forecast || tidecast.forecast.bind(this,"Galway_Port");
+
+window.ido.mi.tides.sites = {
+  galway: "Galway_Port",
+  dublin: "Dublin_Port",
+  howth: "Howth_Harbour",
+  killybegs: "Killybegs_Port",
+  malinhead: "Malin_Head",
+  ballyglass: "Ballyglass",
+  aranmore: "Aranmore",
+  ballycotton: "Ballycotton",
+};
+{
+  var keys = Object.keys(window.ido.mi.tides.sites);
+  for(var i=0;i<keys.length;i++){
+    window.ido.mi.tides[keys[i]] = window.ido.mi.tides[keys[i]] || {};
+    window.ido.mi.tides[keys[i]].forecast = window.ido.mi.tides[keys[i]].forecast || tidecast.forecast.bind(this,window.ido.mi.tides.sites[keys[i]]);
+  }
+}
+
 
 const waves = require('./mi-waves-widget');
 window.ido.mi.waves = window.ido.mi.waves || {};
